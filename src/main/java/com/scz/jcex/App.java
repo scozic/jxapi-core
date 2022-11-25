76e1aea96f2d0cdbb75184f805a4c13e613d5d63
Sylvestre COZIC <sylvestre.cozic@gmail.com>
2022-11-25 01:59:35 +0100

    Project init - added .gitignore

---
+++ src/main/java/com/scz/jcex/App.java
@@ -0,0 +1,13 @@
+package com.scz.jcex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
