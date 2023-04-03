eab6f8a42eca68efeb27cf9aad39370543f1516e
scozic <sylvestre.cozic@gmail.com>
2023-04-04 00:55:44 +0200

    :coffin: Trivial: Unused import removal

--- src/main/java/com/scz/jcex/exchanges/binance/net/BinancePublicApiRestEndpointFactory.java
+++ src/main/java/com/scz/jcex/exchanges/binance/net/BinancePublicApiRestEndpointFactory.java
@@ -3,7 +3,6 @@ package com.scz.jcex.exchanges.binance.net;
 import com.scz.jcex.netutils.deserialization.MessageDeserializer;
 import com.scz.jcex.netutils.rest.RestEndpoint;
 import com.scz.jcex.netutils.rest.RestEndpointFactory;
-import com.scz.jcex.netutils.rest.javaxnet.JavaxNetRestEndpoint;
 
 public class BinancePublicApiRestEndpointFactory implements RestEndpointFactory {
 
