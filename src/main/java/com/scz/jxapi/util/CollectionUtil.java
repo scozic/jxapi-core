8a16a5420d6c1fd27c015425b3455404f6ead20f
scozic <sylvestre.cozic@gmail.com>
2025-02-20 16:42:55 +0100

    :sparkles: New methods in CollectionUtil for cloning / deep cloning lists and maps
    Will be used in upcoming deepClone() method for generated POJOs

--- src/main/java/com/scz/jxapi/util/CollectionUtil.java
+++ src/main/java/com/scz/jxapi/util/CollectionUtil.java
@@ -1,6 +1,12 @@
 package com.scz.jxapi.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.UnaryOperator;
 
 /**
  * Helper static methods around {@link Collection}
@@ -19,5 +25,69 @@ public class CollectionUtil {
 			return true;
 		}
 		return collection.isEmpty();
+	}
+	
+	/**
+	 * Return a copy of the list with the same items.
+	 * @param <T>
+	 * @param list the list to clone
+	 * @return a copy of the list with the same items
+	 */
+	public static <T> List<T> cloneList(List<T> list) {
+		if (list == null) {
+			return null;
+		}
+		return new ArrayList<>(list);
+	}
+	
+	/**
+	 * Return a deep copy of the list with items from input list deep cloned using the provided function.
+	 * @param <T> The type of the items in the list
+	 * @param list the list to deep clone
+	 * @param itemDeepCloneFunction the function to deep clone the items
+	 * @return A deep copy of the list.
+	 */
+	public static <T> List<T> deepCloneList(List<T> list, UnaryOperator<T> itemDeepCloneFunction) {
+		List<T> clonedList = null;
+		if (list != null) {
+			clonedList = new ArrayList<>(list.size());
+			for (T item : list) {
+				clonedList.add(itemDeepCloneFunction.apply(item));
+			}
+		}
+		return clonedList;
+	}
+	
+	/**
+	 * Return a copy of the map with the same items.
+	 * @param <T> The type of the items in the map
+	 * @param map the map to clone
+	 * @return a copy of the map with the same items
+	 */
+	public static <T> Map<String, T> cloneMap(Map<String, T> map) {
+		if (map == null) {
+			return null;
+		}
+		Map<String, T> m = new HashMap<>();
+		m.putAll(map);
+		return m;
+	}
+	
+	/**
+	 * Return a deep copy of the map with items from input map deep cloned using the provided function.
+	 * @param <T> The type of values in the map
+	 * @param map the map to deep clone
+	 * @param itemDeepCloneFunction the function to deep clone the values
+	 * @return
+	 */
+	public static <T> Map<String, T> deepCloneMap(Map<String, T> map, UnaryOperator<T> itemDeepCloneFunction) {
+		Map<String, T> clonedMap = null;
+		if (map != null) {
+			clonedMap = new HashMap<>(map.size());
+			for (Entry<String, T> entry : map.entrySet()) {
+				clonedMap.put(entry.getKey(), itemDeepCloneFunction.apply(entry.getValue()));
+			}
+		}
+		return clonedMap;
 	}
 }
